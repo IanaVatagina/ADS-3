cec5574ad401cf39b5a631645efab87b0f3ec00e
IanaVatagina <vatagina.iana@gmail.com>
2024-03-15 19:44:27 +0300

    Update Project

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,15 +5,23 @@ int cbinsearch(int *arr, int size, int value) {
     int lowInd = 0;
     int highInd = size-1;
     while (lowInd <= highInd) {
-        int middleInd = lowInd+(highInd-lowInd)/2;
+        int middleInd = (highInd+lowInd)/2;
         if (arr[middleInd] > value) {
             highInd = middleInd-1;
         } else if (arr[middleInd] < value) {
             lowInd = middleInd+1;
         } else {
             counter++;
-            lowInd = middleInd+1;
-            highInd = middleInd-1;
+            int nowInd = middleInd-1;
+            while (arr[middleInd] == value) {
+                middleInd++;
+                counter++;
+            }
+            while (arr[nowInd] == value) {
+                nowInd--;
+                counter++;
+            }
+            break;
         }
     }
     return counter;
